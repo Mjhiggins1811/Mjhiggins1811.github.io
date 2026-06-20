03bec9d28b9c1b81b7c4b098539cb4bddfc169c8
Matt Higgins <mjhiggins1811@outlook.com>
2026-06-20 17:10:49 +0100

    initial commi t

---
+++ site.js
@@ -0,0 +1,40 @@
+"use strict";
+// === add header items ===
+// get the header
+let mainHeader = document.getElementById("headerItemContainer");
+// define header items
+let headerItems = [
+    { "name": "list", "link": "lists.html" },
+    { "name": "about", "link": "about.html" }
+];
+// define function to assign to the event listener
+let clickFunction = function setHtmlOfID(id, headerItem, column) {
+    let mainContent = document.getElementById(id);
+    mainContent.innerHTML = "<object data=\"" + headerItem.link + "\"></object>";
+    let headerLine = document.getElementById("mainHeaderLine");
+    headerLine.style.gridColumn = column;
+};
+// set the header item grid up with the right amount of columns
+let headerGrid = document.getElementById("headerItemContainer");
+let gridPropertiesColumns = "";
+for (let i = 0; i < headerItems.length; i++) {
+    gridPropertiesColumns += " auto";
+}
+headerGrid.style.gridTemplateColumns = gridPropertiesColumns;
+let gridColumnCounter = 1;
+// made the header elements with the links and name from the headerItems list
+headerItems.forEach(element => {
+    // make a header html and add it to the header
+    let html = "<a class=\"headerLink\" id=\"" + element.name + "Header\" style=\"cursor: pointer;\">" + element.name + "</a>";
+    mainHeader.insertAdjacentHTML("beforeend", html);
+    // get the element we just made
+    let newHeader = document.getElementById(element.name + "Header");
+    // set the column (this is set here and not left as auto so we can set the grid column of the line in the click event)
+    newHeader.style.setProperty("grid-column", gridColumnCounter.toString());
+    gridColumnCounter += 1;
+    // add the click event
+    newHeader.addEventListener("click", function () {
+        clickFunction("mainContent", element, getComputedStyle(newHeader).gridColumn);
+    });
+});
+//# sourceMappingURL=site.js.map
